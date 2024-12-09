65df0f705af2e6b47fe673378dadc0bab7c78c1c
Marcin Bator <marcinbator.ofc@gmail.com>
2024-12-09 13:47:40 +0100

    refactor: #main day9

--- src/solutions/day9/Day9.java
+++ src/solutions/day9/Day9.java
@@ -7,6 +7,40 @@ import java.util.*;
 
 public class Day9 implements Solution {
     public SolutionResponse solve(Scanner inputScanner) {
+        var memoryList = readMemory(inputScanner);
+        long checksum1 = countPart1(new ArrayList<>(memoryList));
+
+        var memoryBlockList = getMemoryBlocks(memoryList);
+        for (int i = memoryBlockList.size() - 1; i >= 0; i--) {
+            var block = memoryBlockList.get(i);
+            if (!block.value.equals(".")) {
+                i = swapMemory(i, memoryBlockList, block);
+            }
+        }
+
+        long checksum2 = 0;
+        int index2 = 0;
+        for (MemBlock block : memoryBlockList) {
+            if (!block.value.equals(".")) {
+                for (int j = 0; j < block.size; j++) {
+                    checksum2 += Long.parseLong(block.value) * index2;
+                    index2++;
+                }
+            } else {
+                for (int j = 0; j < block.size; j++) {
+                    index2++;
+                }
+            }
+        }
+
+        if (checksum1 != 6446899523367L || checksum2 != 6478232739671L) System.out.println("wrong");
+        return new SolutionResponse(checksum1, checksum2);
+    }
+
+    record MemBlock(int startPos, int size, String value) {
+    }
+
+    private List<String> readMemory(Scanner inputScanner) {
         StringBuilder memory = new StringBuilder();
         long index = 0;
 
@@ -24,37 +58,40 @@ public class Day9 implements Solution {
                         memory.append(".").append(" ");
                     }
                 }
-                memory.append(" ");
             }
         }
 
-        String memoryString = memory.toString().trim();
-        var memoryList = new ArrayList<>(Arrays.stream(memoryString.split(" ")).filter(m -> !m.isEmpty()).toList());
-        var memoryListCopy = new ArrayList<>(memoryList);
-        int replaceIndex = memoryListCopy.size() - 1;
-        for (int i = 0; i < memoryListCopy.size(); i++) {
-            if (memoryListCopy.get(i).equals(".")) {
-                while (memoryListCopy.get(replaceIndex).equals(".") && replaceIndex > i) {
+        return new ArrayList<>(Arrays.asList(memory.toString().trim().split(" ")));
+    }
+
+    private long countPart1(ArrayList<String> memoryList) {
+        int replaceIndex = memoryList.size() - 1;
+        for (int i = 0; i < memoryList.size(); i++) {
+            if (memoryList.get(i).equals(".")) {
+                while (memoryList.get(replaceIndex).equals(".") && replaceIndex > i) {
                     replaceIndex--;
                 }
-                memoryListCopy.set(i, memoryListCopy.get(replaceIndex));
-                memoryListCopy.set(replaceIndex, ".");
+                memoryList.set(i, memoryList.get(replaceIndex));
+                memoryList.set(replaceIndex, ".");
             }
         }
 
         long checkSum = 0;
-        for (int i = 0; i < memoryListCopy.size(); i++) {
-            if (!memoryListCopy.get(i).equals(".")) {
-                long digit = Long.parseLong(memoryListCopy.get(i));
+        for (int i = 0; i < memoryList.size(); i++) {
+            if (!memoryList.get(i).equals(".")) {
+                long digit = Long.parseLong(memoryList.get(i));
                 checkSum += digit * i;
             }
         }
+        return checkSum;
+    }
 
+    private List<MemBlock> getMemoryBlocks(List<String> memoryList) {
         var memoryBlockList = new ArrayList<MemBlock>();
-        int size = 0;
+        int size = 1;
         int startPos = 0;
         String value = memoryList.getFirst();
-        for (int i = 0; i < memoryList.size(); i++) {
+        for (int i = 1; i < memoryList.size(); i++) {
             if (memoryList.get(i).equals(value)) {
                 size++;
             } else {
@@ -66,50 +103,30 @@ public class Day9 implements Solution {
         }
         memoryBlockList.add(new MemBlock(startPos, size, value));
 
-        for (int i = memoryBlockList.size() - 1; i >= 0; i--) {
-            var block = memoryBlockList.get(i);
-            if (block.value.equals(".")) continue;
-            for (int j = 0; j < i; j++) {
-                if (memoryBlockList.get(j).size >= block.size && memoryBlockList.get(j).value.equals(".")) {
-                    var freeBlock = memoryBlockList.get(j);
-                    var b1 = new MemBlock(freeBlock.startPos, block.size, block.value);
-                    var b2 = new MemBlock(freeBlock.startPos + block.size, freeBlock.size - block.size, ".");
-                    memoryBlockList.set(i, new MemBlock(block.startPos, block.size, "."));
-                    memoryBlockList.set(j, b1);
-//                    if(b2.size > 0){
-                    memoryBlockList.add(b2);
-                    memoryBlockList.sort(Comparator.comparingInt(bl -> bl.startPos));
-                    i++;
-//                    }
-                    i -= mergeGaps(memoryBlockList);
-                    break;
-                }
-            }
-        }
-
-        long checksum2 = 0;
-        int index2 = 0;
-        for (int i = 0; i < memoryBlockList.size(); i++) {
-            var block = memoryBlockList.get(i);
-            if (!block.value.equals(".")) {
-                for (int j = 0; j < block.size; j++) {
-                    checksum2 += Long.parseLong(block.value) * index2;
-                    index2++;
-                }
-            } else {
-                for (int j = 0; j < block.size; j++) {
-                    index2++;
-                }
-            }
-        }
-
-        return new SolutionResponse(checkSum, checksum2);
+        return memoryBlockList;
     }
 
-    record MemBlock(int startPos, int size, String value) {
+    private int swapMemory(int i, List<MemBlock> memoryBlockList, MemBlock block) {
+        for (int j = 0; j < i; j++) {
+            if (memoryBlockList.get(j).size < block.size || !memoryBlockList.get(j).value.equals(".")) continue;
+
+            var freeBlock = memoryBlockList.get(j);
+            var b1 = new MemBlock(freeBlock.startPos, block.size, block.value);
+            var b2 = new MemBlock(freeBlock.startPos + block.size, freeBlock.size - block.size, ".");
+            memoryBlockList.set(i, new MemBlock(block.startPos, block.size, "."));
+            memoryBlockList.set(j, b1);
+            memoryBlockList.add(b2);
+            i++;
+
+            memoryBlockList.sort(Comparator.comparingInt(bl -> bl.startPos));
+            i -= mergeFreeSpaces(memoryBlockList);
+
+            break;
+        }
+        return i;
     }
 
-    int mergeGaps(List<MemBlock> list) {
+    private int mergeFreeSpaces(List<MemBlock> list) {
         int count = 0;
         for (int i = 0; i < list.size() - 1; i++) {
             if (list.get(i).value.equals(".") && list.get(i + 1).value.equals(".")) {
@@ -122,4 +139,3 @@ public class Day9 implements Solution {
         return count - 1;
     }
 }
-
